41885f8ec02719e8beda9be2741d233f50464640
Bill Cawthra <bcawthra@gmail.com>
2016-10-08 11:59:39 -0400

    updated environment.sh for release of ansible-container

--- ansible-container/environment.sh
+++ ansible-container/environment.sh
@@ -2,23 +2,13 @@
 if [ ! -f ./env/bin/activate ]; then
   virtualenv env
   source ./env/bin/activate
-  pip install -U boto boto3 ansible pip-review setuptools
+  pip install -U boto boto3 ansible pip-review setuptools ansible-container
 #  pip install -U ansible-container
-else
-  source ./env/bin/activate
 fi
-
-if [ ! -d ansible-container ]; then
-  git clone https://github.com/ansible/ansible-container.git
-fi
-
-cd ansible-container
-git pull
-python ./setup.py develop
 
 echo "ansible-container installed and configured."
-echo "Setting exports... fixes some timeouts."
-export DOCKER_CLIENT_TIMEOUT=120
-export COMPOSE_HTTP_TIMEOUT=120
+#echo "Setting exports... fixes some timeouts."
+#export DOCKER_CLIENT_TIMEOUT=120
+#export COMPOSE_HTTP_TIMEOUT=120
 
 echo "run 'source ./env/bin/activate'."
