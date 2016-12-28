f4ed4b96a70fbb8a0791d9a4debbd0ef6d2b0429
Bill Cawthra <bcawthra@gmail.com>
2016-12-28 14:04:21 -0500

    removed ansible-container directory.  now in the containers repo

--- ansible-container/devcontainers/aws-run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-docker run \
-  -dt \
-  -h awsdev  \
-  --name awsdev \
-  -v $HOME/.ssh:/home/aws-user/.ssh \
-  -v $HOME/gits:/home/aws-user/gits \
-  ansible-container-awsdev /bin/bash
